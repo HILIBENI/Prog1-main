5f92589436c6b9ea2541a6f80d40e9bc677d0a47
Bhzilhinger <beni.hilzinger@gmail.com>
2023-03-31 10:55:19 +0200

    comments

--- Own Projects/Python/strfind.py
+++ Own Projects/Python/strfind.py
@@ -16,7 +16,7 @@ def numgen(rl,randstep):
     genstart = time.time()
     rstr = ''
     for i in range(rl):
-         rstr += str(random.randrange(randstep))
+         rstr += str(random.randrange(randstep)) #generate random number and convert to string
     genend = time.time()
     print(f"Generating {round((len(rstr)/1000000),2)}M numbers took {round((genend-genstart),3)} seconds")
     return rstr
@@ -26,14 +26,14 @@ def numgen(rl,randstep):
 def convert():
     numl = ''
     for i in strf:
-          numl += str(ord(i))
+          numl += str(ord(i)) #ord() function basically returns the ASCII number value of a char
     return numl
 
 def fstr(substr, fullstr,i):
-    strindex = str(fullstr).find(str(substr))
+    strindex = str(fullstr).find(str(substr)) #working with strings here instead of numbers so this is easier
     if strindex == -1:
         print(f"your number sequence was not found within the specified larger set, trying again.")
-        main(i)
+        main(i) #in case substr is not found, restart main() with a larger set.
     else:
         end = time.time()
         print(f"your specified number sequence starts at: {strindex} and took {round((end-start),3)} seconds to find")
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     start = time.time()
     strf = str(input("the string you want to find: "))
     iterations = 5000000
-    curve = 100000
+    curve = 100000 #how fast the amount of numbers genrated should increase
     print(f"Translated \"{strf}\" to {convert()}.")
     main(iterations)
 
